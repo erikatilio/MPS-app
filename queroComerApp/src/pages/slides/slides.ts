cbb82deb17c971164cd2014861518c580bdb4b86
erikatilio <erimom3000@gmail.com>
2018-11-20 18:17:10 -0400

    Brabo

---
+++ queroComerApp/src/pages/slides/slides.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { HomePage } from '../home/home';
+
+@IonicPage()
+@Component({
+  selector: 'page-slides',
+  templateUrl: 'slides.html',
+})
+export class SlidesPage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad SlidesPage');
+  }
+
+  goToHomePage(){
+    this.navCtrl.push(HomePage);
+    this.navCtrl.setRoot(HomePage);
+  }
+
+}
