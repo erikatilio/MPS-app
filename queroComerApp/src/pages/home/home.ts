f0313b609271f12e7ac94eea3a8e28474e3c3274
erikatilio <erimom3000@gmail.com>
2018-12-01 02:33:06 -0200

    Base receitas

--- queroComerApp/src/pages/home/home.ts
+++ queroComerApp/src/pages/home/home.ts
@@ -9,19 +9,37 @@ import { LoginPage } from '../login/login';
   templateUrl: 'home.html'
 })
 export class HomePage {
-  constructor(public navCtrl: NavController) {
+  public listaReceitas: Array<object> = [];
 
+  constructor(public navCtrl: NavController) {
+    this.listaReceitas = [
+      {
+        "nome": "Sorvete 1",
+        "descricao": " Sorvete ou gelado é uma sobremesa gelada à base de lacticínios.",
+        "imagem": "https://www.bonde.com.br/img/galeriasocial/img_gal2_18355.jpg"
+      },
+      {
+        "nome": "Sorvete 2",
+        "descricao": " Sorvete ou gelado é uma sobremesa gelada à base de lacticínios.",
+        "imagem": "http://g.glbimg.com/og/gs/gsat5/f/thumbs/tag/2015/08/27/sorvete-620.jpg"
+      },
+      {
+        "nome": "Sorvete 3",
+        "descricao": " Sorvete ou gelado é uma sobremesa gelada à base de lacticínios.",
+        "imagem": "https://www.comidaereceitas.com.br/images/stories/2012/11/sorvete_cremoso_baunilha.jpg"
+      }
+    ]
   }
 
-  goToFavoritosPage(){
+  goToFavoritosPage() {
     this.navCtrl.push(FavoritosPage);
   }
 
-  goToSobrePage(){
+  goToSobrePage() {
     this.navCtrl.push(SobrePage);
   }
 
-  goToLoginPage(){
+  goToLoginPage() {
     this.navCtrl.setRoot(LoginPage);
   }
 
