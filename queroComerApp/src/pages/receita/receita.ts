c655ecee08eeab361e7151a814e984e57a8b88cc
EnriqueIzel2 <eleaoizel@gmail.com>
2018-12-02 00:44:58 -0400

    Corrigindo funções

--- queroComerApp/src/pages/receita/receita.ts
+++ queroComerApp/src/pages/receita/receita.ts
@@ -11,33 +11,50 @@ export class ReceitaPage {
   public titulo:string;
   public curtida:number;
   public descurtida:number;
-  public avaliado:boolean;
+  public avaliado:boolean;       //verifica se já foi avalidado
+  public like:boolean;          //verifica se já foi curtido
+  public deslike:boolean;       //verifica se já foi descurtido
+
   constructor(public navCtrl: NavController, public navParams: NavParams) {
     this.titulo = "Titulo da Receita";
     this.curtida = 0;
     this.descurtida = 0;
     this.avaliado = false;
+    this.like = false;
+    this.deslike = false;
   }
 
   curtir(){
-    if(this.avaliado==false){
+    if(this.avaliado == false){
       this.curtida++;
-      this.descurtida=0;
       this.avaliado = true;
-    }else if(this.descurtida==0){
+      this.like = true;
+    }else if(this.avaliado == true && this.like == true){
       this.curtida--;
       this.avaliado = false;
+      this.like = false;
+    }
+    else if (this.avaliado == true && this.like == false) {
+      this.curtida++;
+      this.like = true;
+      this.deslike = false;
     }
   }
 
   descurtir(){
-    if(this.avaliado==false){
+    if(this.avaliado == false){
       this.descurtida++;
-      this.curtida=0;
       this.avaliado = true;
-    }else if(this.curtida==0){
+      this.deslike = true;
+    }else if(this.avaliado == true && this.deslike == true){
       this.descurtida--;
       this.avaliado = false;
+      this.deslike = false;
+    }
+    else if (this.avaliado == true && this.deslike == false) {
+      this.descurtida++;
+      this.deslike = true;
+      this.like = false;
     }
   }
 
