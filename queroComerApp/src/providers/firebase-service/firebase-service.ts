4c7fdb4f0c88660c15afccc460d578312c5710bb
EnriqueIzel2 <eleaoizel@gmail.com>
2018-12-03 21:20:32 -0400

    diretorio para parte de banco de dados

---
+++ queroComerApp/src/providers/firebase-service/firebase-service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { AngularFireDatabase } from 'angularfire2/database';
+
+
+/*
+  Generated class for the FirebaseServiceProvider provider.
+
+  See https://angular.io/guide/dependency-injection for more info on providers
+  and Angular DI.
+*/
+@Injectable()
+export class FirebaseServiceProvider {
+
+  constructor(public db: AngularFireDatabase) {
+    console.log('Hello FirebaseServiceProvider Provider');
+  }
+
+}
