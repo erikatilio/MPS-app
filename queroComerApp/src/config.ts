95f0e8dc8c109fcb0326cd5bcb308b67ba0f3121
EnriqueIzel2 <eleaoizel@gmail.com>
2018-12-04 00:20:28 -0400

    mudança da conta do banco de dados

--- queroComerApp/src/config.ts
+++ queroComerApp/src/config.ts
@@ -1,8 +1,8 @@
 export const config = {
-	apiKey: "AIzaSyBhhebvljg6fq-kCUS1a2xCLtf-HCQepPM",
-	authDomain: "cadereceita.firebaseapp.com",
-	databaseURL: "https://cadereceita.firebaseio.com",
-	projectId: "cedereceita",
-	storageBucket: "cadereceita.appspot.com",
-	messagingSenderId: "30954215250"
+	apiKey: "AIzaSyDinttKE1Cgp-M8lijaJOtYXJlWFHGGA0w",
+    authDomain: "cade-a-receita.firebaseapp.com",
+    databaseURL: "https://cade-a-receita.firebaseio.com",
+    projectId: "cade-a-receita",
+    storageBucket: "cade-a-receita.appspot.com",
+    messagingSenderId: "478423410018"
 };
