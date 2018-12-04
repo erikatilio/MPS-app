cfdd9d8fe5b2aca8a53da6ec3fcd8008a619f9d4
EnriqueIzel2 <eleaoizel@gmail.com>
2018-12-03 22:17:05 -0400

    adicionando função(ainda incompleta) e importações

--- queroComerApp/src/pages/cadastro/cadastro.ts
+++ queroComerApp/src/pages/cadastro/cadastro.ts
@@ -1,5 +1,12 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { HomePage } from '../home/home';
+
+//importação do firebase
+import { FirebaseServiceProvider } from '../../providers/firebase-service/firebase-service';
+
+//importação de interface
+import { Receita } from '../../model/receita';
 
 
 @IonicPage()
@@ -8,8 +15,29 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'cadastro.html',
 })
 export class CadastroPage {
+	public receitas:any;
+	public receita = {} as Receita;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public dbService: FirebaseServiceProvider) {
+  	this.receitas = "";
+  }
+
+  cadastrar(receita:Receita) {
+  	try {
+  		this.receitas = firebase.database().ref("receitas");
+  		this.receitas.push().set(
+  			{
+  				nome: this.receita.nome,
+	  			ingredientes: this.receita.ingredientes,
+	  			modoPreparo: this.receita.modoPreparo
+	  		}
+  		);
+  		alert("Receita cadastrada");
+    } catch (e) {
+        alert("Não foi possível cadastrar a receita, tente mais tarde");
+    }
+
+  	this.navCtrl.setRoot(HomePage);
   }
 
   ionViewDidLoad() {
