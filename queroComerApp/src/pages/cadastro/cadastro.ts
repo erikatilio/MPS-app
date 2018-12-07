e126277a4ec0ca97da133fd06b444e9d67277f95
erikatilio <erimom3000@gmail.com>
2018-12-06 23:36:35 -0200

    Barra de pesquisa e responsivo das page feito! :)

--- queroComerApp/src/pages/cadastro/cadastro.ts
+++ queroComerApp/src/pages/cadastro/cadastro.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { HomePage } from '../home/home';
+import { AlertController } from 'ionic-angular';
 
 //importação do firebase
 import { FirebaseServiceProvider } from '../../providers/firebase-service/firebase-service';
@@ -15,23 +16,32 @@ import { Receita } from '../../model/receita';
   templateUrl: 'cadastro.html',
 })
 export class CadastroPage {
-	public receitas:any;
-	public receita = {} as Receita;
+  public receitas: any;
+  public receita = {} as Receita;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, public dbService: FirebaseServiceProvider) {
-  	this.receitas = "";
+  constructor(public navCtrl: NavController, public navParams: NavParams, public dbService: FirebaseServiceProvider, private alertCtrl: AlertController) {
+    this.receitas = "";
   }
 
-//função para cadastrar receita
-  cadastrar(receita:Receita) {
-  	try {
+  public menssagem(title, message) {
+    let al = this.alertCtrl.create({
+      title: title,
+      subTitle: message,
+      buttons: ['OK']
+    });
+    al.present();
+  }
+
+  //função para cadastrar receita
+  cadastrar(receita: Receita) {
+    try {
       this.dbService.save(receita);  //chama a função
-  		alert("Receita cadastrada");
+      this.menssagem("Parabéns", "Sua receita foi cadastrada com sucesso.");
     } catch (e) {
-        alert("Não foi possível cadastrar a receita, tente mais tarde");
+      this.menssagem("Desculpe", "Não foi possivel cadastrar sua receita, tente mais tarde.");
     }
 
-  	this.navCtrl.setRoot(HomePage);
+    this.navCtrl.setRoot(HomePage);
   }
 
   ionViewDidLoad() {
--- queroComerApp/src/pages/home/home.ts
+++ queroComerApp/src/pages/home/home.ts
@@ -11,27 +11,33 @@ import { CadastroPage } from '../cadastro/cadastro';
   templateUrl: 'home.html'
 })
 export class HomePage {
-  public listaReceitas: Array<object> = [];
+  public listaReceitas: any;//array com a lista de receitas
 
   constructor(public navCtrl: NavController) {
+    this.gerarListaReceitas();//chama a lista de receitas no construtor para que seja atualizada na home
+  }
+
+  //fução que gera a lista de receitas
+  gerarListaReceitas() {
     this.listaReceitas = [
       {
-        "nome": "Sorvete",
-        "descricao": " Sorvete ou gelado é uma sobremesa gelada à base de lacticínios.",
-        "imagem": "https://www.bonde.com.br/img/galeriasocial/img_gal2_18355.jpg"
+        nome: "Sorvete",
+        descricao: " Sorvete ou gelado é uma sobremesa gelada à base de lacticínios.",
+        imagem: "https://www.bonde.com.br/img/galeriasocial/img_gal2_18355.jpg"
       },
       {
-        "nome": "X-Tudo",
-        "descricao": " Sanduíche de carne temperada dentro do pão, como cebola, alface, tomate, maionese, ketchup, queijo e bacon e ovos.",
-        "imagem": "https://media-cdn.tripadvisor.com/media/photo-s/0b/ed/c8/10/x-tudo.jpg"
+        nome: "X-Tudo",
+        descricao: " Sanduíche de carne temperada dentro do pão, como cebola, alface, tomate, maionese, ketchup, queijo e bacon e ovos.",
+        imagem: "https://media-cdn.tripadvisor.com/media/photo-s/0b/ed/c8/10/x-tudo.jpg"
       },
       {
-        "nome": "Feijoada",
-        "descricao": " Um guisado de feijão com carne bovina e carne de porco, servido com arroz.",
-        "imagem": "http://esbrasil.com.br/wp-content/uploads/2018/04/Feijoada-696x443.jpg"
+        nome: "Feijoada",
+        descricao: " Um guisado de feijão com carne bovina e carne de porco, servido com arroz.",
+        imagem: "http://esbrasil.com.br/wp-content/uploads/2018/04/Feijoada-696x443.jpg"
       }
-    ]
+    ];
   }
+
   //função que carrega a pagina favoritos
   goToFavoritosPage() {
     this.navCtrl.push(FavoritosPage);
@@ -54,4 +60,22 @@ export class HomePage {
     this.navCtrl.push(CadastroPage);
   }
 
+  //Função que filtar receitas pegando por parametro o input do serachbar
+  filtrarReceita(event: any) {
+    this.gerarListaReceitas();//atualiza a lista de receitas
+    let searchTerm = event.target.value;
+
+    //filtra oque foi digitado no seacrh bar na listaDeReceitas
+    if (searchTerm && searchTerm.trim() != '') {
+      this.listaReceitas = this.listaReceitas.filter((item) => {
+        return (item.nome.toLowerCase().indexOf(searchTerm.toLowerCase()) > - 1);
+      })
+    }
+
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad HomePage');
+  }
+  
 }
